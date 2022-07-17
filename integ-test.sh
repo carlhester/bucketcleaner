4048037b88093c62124d17531edec26c5f9e180b
Carl Hester <crucialcarl@gmail.com>
2022-07-17 15:26:28 -0700

    space

--- integ-test.sh
+++ integ-test.sh
@@ -8,14 +8,14 @@ UUID=`uuidgen`
 BUCKETNAME=bucketcleaner-${TS}-${UUID:0:16}
 aws s3 mb s3://${BUCKETNAME}
 result=$?
-if [$result -ne 0]; then
+if [ $result -ne 0 ]; then
     exit 1
 fi
 
 # add bucket versioning
 aws s3api put-bucket-versioning --bucket ${BUCKETNAME} --versioning-configuration Status=Enabled
 result=$?
-if [$result -ne 0]; then
+if [ $result -ne 0 ]; then
     exit 1
 fi
 
@@ -23,51 +23,51 @@ fi
 uuidgen > outfile
 aws s3 cp outfile s3://${BUCKETNAME}
 result=$?
-if [$result -ne 0]; then
+if [ $result -ne 0 ]; then
     exit 1
 fi
 
 uuidgen > outfile
 aws s3 cp outfile s3://${BUCKETNAME}
 result=$?
-if [$result -ne 0]; then
+if [ $result -ne 0 ]; then
     exit 1
 fi
 
 uuidgen > outfile
 aws s3 cp outfile s3://${BUCKETNAME}
 result=$?
-if [$result -ne 0]; then
+if [ $result -ne 0 ]; then
     exit 1
 fi
 
 # whats in the bucket
 aws s3 ls s3://${BUCKETNAME}
 result=$?
-if [$result -ne 0]; then
+if [ $result -ne 0 ]; then
     exit 1
 fi
 
 go mod tidy
 result=$?
-if [$result -ne 0]; then
+if [ $result -ne 0 ]; then
     exit 1
 fi
 
 go build -o bin ./main.go
 result=$?
-if [$result -ne 0]; then
+if [ $result -ne 0 ]; then
     exit 1
 fi
 
 ./bin ${BUCKETNAME} us-west-1 really
 result=$?
-if [$result -ne 0]; then
+if [ $result -ne 0 ]; then
     exit 1
 fi
 
 aws s3 ls s3://${BUCKETNAME}
 result=$?
-if [$result -eq 1]; then
+if [ $result -eq 1 ]; then
     exit 0
 fi
